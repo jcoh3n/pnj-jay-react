4bbdeec1269f5e75dc994b44c27d35637fb67577
jcohen <jcohen@student.42.fr>
2024-12-02 19:23:57 +0100

    ajout d'alias @

--- babel.config.js
+++ babel.config.js
@@ -4,13 +4,27 @@ module.exports = function (api) {
     presets: ['babel-preset-expo'],
     plugins: [
       'react-native-reanimated/plugin',
-      ["module:react-native-dotenv", {
-        "moduleName": "@env",
-        "path": ".env",
-        "blacklist": null,
-        "whitelist": null,
-        "safe": false,
-        "allowUndefined": true
+      ['module:react-native-dotenv', {
+        moduleName: '@env',
+        path: '.env',
+        safe: false,
+        allowUndefined: true
+      }],
+      ['module-resolver', {
+        root: ['./'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@': './src',
+          '@components': './src/components',
+          '@screens': './src/screens',
+          '@navigation': './src/navigation',
+          '@hooks': './src/hooks',
+          '@services': './src/services',
+          '@stores': './src/stores',
+          '@utils': './src/utils',
+          '@constants': './src/constants',
+          '@types': './src/types'
+        }
       }]
     ]
   };
